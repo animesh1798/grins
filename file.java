4f30d096087d0388881535ec9b849c0395686465
animesh1798 <79442246+animesh1798@users.noreply.github.com>
2021-02-22 13:12:31 +0530

    Add files via upload

---
+++ file.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+Class College{
+	int id, nos;
+	String name, year, city, state, country;
+	List<String> courses;
+}
+
+Class Student{
+	int id;
+	String name, year, cid, skills;
+	}
+
+Class Main{
+	public static void main(){
+	;
+	}
+}
